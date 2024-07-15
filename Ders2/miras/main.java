b9297599718e1ac976a7d57e16e3205d36c89928
hackerFaruk <onuralparslan97@gmail.com>
2024-07-15 12:17:00 +0300

    extensions

--- Ders2/miras/main.java
+++ Ders2/miras/main.java
@@ -1,26 +1,51 @@
-public class main{
+public class main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("test");
     }
 
-
 }
 
-class Parent{
+class Parent {
     String name;
     String surname;
 
-    public Parent(String name, String surname){
+    public Parent(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
 
-    public String GetFullName(){
-        return this.name + " " + this.surname ;
+    public String GetFullName() {
+        return this.name + " " + this.surname;
     }
 
-    public void introduce(){
-        System.out.println( this.GetFullName());
+    public void introduce() {
+        System.out.println(" Merhaba, ben " + this.GetFullName());
     }
+
+    public void dinner() {
+        System.out.println("yemek hazır");
+
+    }
+}
+
+class Child extends Parent {
+    String favGame;
+
+    public Child(String name, String surname, String favGame) {
+        super(name, surname);
+        this.favGame = favGame;
+    }
+
+    // overloaded
+    public Child(String name, String surname) {
+        super(name, surname);
+        this.favGame = "fortnite";
+    }
+
+    @Override
+    public void dinner() {
+        System.out.println(this.favGame + " oynuyorum bitsin gelcem");
+    }
+
 }
