681bbe3f5e2d4dbcb59cffb4d0776d79cb4c7eeb
hackerFaruk <onuralparslan97@gmail.com>
2024-07-24 20:18:16 +0300

    ders notları 3

--- Ders3/loops/loops.java
+++ Ders3/loops/loops.java
@@ -1,5 +1,5 @@
 
-public class main {
+public class loops {
 
     public static void main(String[] args) {
 
@@ -8,14 +8,22 @@ public class main {
         if (val > 9) {
             System.out.println("val 9 dan büyük");
         } else if (val == 9) {
-            System.out.println("val 9 değerinde ");
+            System.out.println("val 9 değerine eşittir");
 
         } else {
             System.out.println("val 9 dan küçük");
         }
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 1; i < 16; i = i * 2) {
             System.out.println("current loop is " + i);
+        }
+
+        String[] hello = { "hello", "world" };
+        int[] numbers = { 1, 2, 3, 4, 2 };
+
+        for (String string : hello) {
+            System.out.println(string);
+
         }
 
     }
--- Ders3/terminal/main.java
+++ Ders3/terminal/main.java
@@ -1,7 +1,17 @@
-package Ders3.terminal;
 
 public class main {
 
-    public void static main(String args)
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("bana arguman ver");
+        } else if (args.length > 0) {
+            System.out.println(args[0]);
+            for (String kelime : args) {
+                System.out.println(kelime);
+            }
+
+        }
+
+    }
 
 }
