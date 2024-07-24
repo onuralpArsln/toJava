8668784bdca0117f0a2fd6ea62ca5ced2500899c
hackerFaruk <onuralparslan97@gmail.com>
2024-07-24 20:29:32 +0300

    odev

--- Ders3/terminal/main.java
+++ Ders3/terminal/main.java
@@ -2,6 +2,7 @@
 public class main {
 
     public static void main(String[] args) {
+
         if (args.length == 0) {
             System.out.println("bana arguman ver");
         } else if (args.length > 0) {
