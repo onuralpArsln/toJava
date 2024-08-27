c56275c18fe52ce5ffb9b7e0c8fa2ef5e1c2fd79
hackerFaruk <onuralparslan97@gmail.com>
2024-08-27 23:17:34 +0300

    added how print reacts to classess

--- Ders7/ArrayListsLesson/Main.java
+++ Ders7/ArrayListsLesson/Main.java
@@ -10,6 +10,9 @@ public class Main {
         // .add methodu ile yeni eleman eklemek mümkün
         names.add("Alice");
         names.add("Bob");
+        names.add("Calvin");
+        names.add("Yoda");
+        names.add("Benedict");
 
         // index kullanarak elemanlara ulaşabilirsin bunu .get methodu ile yapacaksın
         System.out.println("First name: " + names.get(0));
@@ -19,12 +22,18 @@ public class Main {
             System.out.println(name);
         }
 
+        System.out.println(names);
         // Seçtiğin ismi silebilirsin
         names.remove("Alice");
+        System.out.println(names);
 
-        // for döngüsü yap
-        for (String name : names) {
-            System.out.println(name);
-        }
+        // bir elementi değiştirme
+        names.set(0, "James");
+        System.out.println(names);
+
+        names.remove(1);
+        System.out.println(names);
+        System.out.println(names.size());
+
     }
 }
--- Ders7/GenericArrayLists/Main.java
+++ Ders7/GenericArrayLists/Main.java
@@ -1,10 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Create a GenericArray for Integers
+        // generic bir array oluşturduk
         GenericArray<Integer> intArray = new GenericArray<>(5);
         intArray.set(0, 10);
         System.out.println(intArray.get(0));
+        System.out.println(intArray);
+        intArray.set(1, 20);
+        intArray.set(2, 30);
+        intArray.set(3, 40);
+        intArray.set(4, 50);
+        System.out.println(intArray);
     }
 }
 
@@ -22,5 +28,18 @@ class GenericArray<T> {
 
     public T get(int index) {
         return array[index];
+    }
+
+    // clasının printe tepkisini değiştiri
+    @Override
+    public String toString() {
+        String result = "[";
+        for (T element : this.array) {
+            result = result + " " + element;
+
+        }
+        result += "]";
+        return result;
+
     }
 }
