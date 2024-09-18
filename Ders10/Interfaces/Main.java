99d92554f5256f847b7f6c02695af24872ae6cfe
hackerFaruk <onuralparslan97@gmail.com>
2024-09-18 21:54:56 +0300

    added interface lesson

---
+++ Ders10/Interfaces/Main.java
@@ -0,0 +1,38 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Basic basicTest = new Basic();
+        Complex complexTest = new Complex();
+        complexTest.value = 132;
+
+        Evaluater.runElement(complexTest);
+        // Evaluater.runElement(basicTest);
+
+    }
+}
+
+interface MySpecialInterface {
+
+    void run();
+}
+
+class Basic {
+    int value;
+}
+
+class Complex implements MySpecialInterface {
+    int value;
+
+    @Override
+    public void run() {
+        System.out.println("This has value of " + this.value);
+    }
+}
+
+class Evaluater {
+
+    static void runElement(MySpecialInterface element) {
+        element.run();
+    }
+}
